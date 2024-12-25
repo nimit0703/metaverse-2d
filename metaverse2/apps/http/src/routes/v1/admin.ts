d05de09d9c8669653e588943399244f2c3dddb1b
nimit0703 <link.nimit@gmail.com>
2024-12-25 23:15:40 +0530

    init

---
+++ metaverse2/apps/http/src/routes/v1/admin.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+export const adminRouter = Router();
+adminRouter.post("/elememt", (req, res) => {});
+adminRouter.put("/element/:elementId", (req, res) => {});
+adminRouter.get("/:avatar", (req, res) => {});
+adminRouter.get("/map", (req, res) => {});
---
+++ metaverse2/apps/http/src/routes/v1/user.ts
@@ -0,0 +1,6 @@
+import { Router } from "express";
+
+export const userRouter = Router();
+
+userRouter.post("/metadata", async (req, res) => {});
+userRouter.get("/metadata/bulk", async (req, res) => {});
