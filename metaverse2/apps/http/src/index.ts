1df79db58af9542173c45edd551201b09efeab03
nimit0703 <link.nimit@gmail.com>
2024-12-26 16:16:04 +0530

    Fixed cross function dependency issue and build issue

--- metaverse2/apps/http/src/index.ts
+++ metaverse2/apps/http/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { router } from "./routes/v1";
-import client from "@repo/db"
+import client from "@repo/db/client"
 const app = express();
 
 app.use("/api/v1", router);
